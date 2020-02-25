46cd420e5944f0f78cfbc801b58604fe527d97d1
redista <sealacquire@gmail.com>
2020-02-25 23:30:46 +0000

    changed stuff

--- pull_most_recent.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-from pprint import pprint
-
-client = MongoClient('mongodb+srv://test:FioTestPass@bindata-lhxe1.mongodb.net/test?retryWrites=true&w=majority')
-db = client.bintestdb
-
-def pull_first_res(db):
-    res = db.bintestdb.find().sort('datetime', -1).limit(1)
-    return res[0]
