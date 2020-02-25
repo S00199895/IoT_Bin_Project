4c161b3bcd20f46722b146b2f5debe3fe1dd6298
redista <sealacquire@gmail.com>
2020-02-25 23:19:33 +0000

    better abstraction on db

--- bin.py
+++ bin.py
@@ -2,12 +2,14 @@ from pymongo import MongoClient
 from pprint import pprint
 from datetime import datetime
 #from bridgeclient import Bridgeclient as bridgeclient
-import push_bin
-import pull_most_recent
+import push_bin as push
+import pull_results as pull
 
 client = MongoClient('mongodb+srv://test:FioTestPass@bindata-lhxe1.mongodb.net/test?retryWrites=true&w=majority')
 db = client.bintestdb
+db = db.bintestdb
+
 #board = bridgeclient()
 
-push_bin.push_bin(50, db)
-pprint(pull_most_recent.pull_first_res(db))
+push.push_bin(50, db)
+pprint(pull.pull_first_res(db))
