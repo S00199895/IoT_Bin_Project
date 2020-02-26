737c7019adc2fcf55b2210cb7cfb05e88d20b2fc
redista <sealacquire@gmail.com>
2020-02-26 00:04:58 +0000

    added arduino files (test)

---
+++ arduino_bin.py
@@ -0,0 +1,25 @@
+#!usr/bin/python
+
+from pymongo import MongoClient
+from pprint import pprint
+from datetime import datetime
+from bridgeclient import Bridgeclient as bridgeclient
+import push_bin as push
+import pull_results as pull
+
+# SET IR SERNSOR TO PIN A1
+# SET MOTION SENSOR TO PIN D3
+IR_SENSOR = 'A1'
+MOTION_SENSOR = 'D3'
+
+client = MongoClient('mongodb+srv://test:FioTestPass@bindata-lhxe1.mongodb.net/test?retryWrites=true&w=majority')
+db = client.bintestdb
+db = db.bintestdb
+
+board = bridgeclient()
+
+while(true):
+    if(board.get('D3') == 1):
+        vol = board.get(IR_SENSOR)
+        push.push_bin(vol, db)
+        pprint(pull.pull_first_res(db))
--- bin.py
+++ bin.py
@@ -1,15 +1,11 @@
 from pymongo import MongoClient
 from pprint import pprint
 from datetime import datetime
-#from bridgeclient import Bridgeclient as bridgeclient
 import push_bin as push
 import pull_results as pull
 
 client = MongoClient('mongodb+srv://test:FioTestPass@bindata-lhxe1.mongodb.net/test?retryWrites=true&w=majority')
-db = client.bintestdb
-db = db.bintestdb
-
-#board = bridgeclient()
+db = client.bintestdb.bintest.db
 
 push.push_bin(50, db)
 pprint(pull.pull_first_res(db))
